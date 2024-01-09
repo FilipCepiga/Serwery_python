fb117585728a0979871afa348cfd43e1ff5da49d
FilipCepiga <filipcepiga@student.agh.edu.pl>
2024-01-09 16:00:28 +0100

    Poprawa finalnej wersji serwerow

--- servers.py
+++ servers.py
@@ -6,7 +6,6 @@ from typing import TypeVar, List, Optional, Dict
 
 import re
 
-
 class Product:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
     def __init__(self, name: str, price: float) -> None:
